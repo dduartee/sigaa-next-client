17bf7f260f50507864d7d8c39e4cc57034171236
Gabriel Kleemann Duarte <gabrielkduarte234@gmail.com>
2021-06-27 11:19:45 -0300

    alteração das funções para envio/receber de eventos & login via cache

--- services/api/client.ts
+++
@@ -1,3 +0,0 @@
-import io from "socket.io-client";
-const client = io("wss://sigaa-socket-api.herokuapp.com/");
-export { client }
--- services/api/socket.ts
+++ services/api/socket.ts
@@ -9,13 +9,19 @@ export const disconnectSocket = () => {
   console.log("Disconnecting socket...");
   if (socket) socket.disconnect();
 }
+export const subscribeAllEvents = (cb: any) => {
+  if (!socket) return (true);
+  socket.onAny((...args) => {
+    return cb(...args)
+  })
+}
 export const subscribeEvent = (name: string, cb: any) => {
   if (!socket) return (true);
   socket.on(name, data => {
-    console.log(`Received ${name} event.`);
+    console.log(`Received ${name} event. With data: ${data}`);
     return cb(data)
   })
 }
-export const sendEvent = (name: string, value?: string) => {
+export const sendEvent = (name: string, value?: any) => {
   if (socket) socket.emit(name, value);
 }
