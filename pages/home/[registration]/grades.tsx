6e96e2914908bf4cf3bc26249e5fda23fbc34f3a
Gabriel Kleemann Duarte <gabrielkduarte234@gmail.com>
2022-06-22 15:54:24 -0300

    tabela de notas com classificação por período

--- pages/home/[registration]/grades.tsx
+++ pages/home/[registration]/grades.tsx
@@ -5,28 +5,16 @@ import useTokenHandler from "@hooks/useTokenHandler";
 import useUserHandler, { emitUserInfo } from "@hooks/useUserHandler";
 import { GetServerSidePropsContext } from "next";
 import useAPIHandler from "@hooks/useAPIEvents";
-import { Bond, Course } from "@types";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell, { tableCellClasses } from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import { styled } from "@material-ui/core/styles";
 import {
   Box,
-  CircularProgress,
-  Collapse,
-  IconButton,
-  Typography,
 } from "@material-ui/core";
-import { KeyboardArrowDown, KeyboardArrowUp } from "@material-ui/icons";
-import CollapsibleTable from "@components/Home/CollapsibleTable";
 import Head from "next/head";
 import useGradesEvents, {
   emitGradesList,
 } from "@hooks/courses/useGradesEvents";
 import useTabHandler from "@hooks/useTabHandler";
 import HomeProviders from "@components/homeProvider";
+import Grades from "@components/Grades/Content";
 
 function InitializeHooks({ registration }: { registration: string }) {
   const [valid, setValid] = useState(true);
@@ -86,7 +74,7 @@ export default function GradesPage({ registration }: { registration: string }) {
       emitUserInfo({ token: localStorage.getItem("token") }, socket);
     } else window.location.href = "/";
   }, [valid]);
-
+  const bond = data[0]
   return (
     <>
       <Head>
@@ -100,7 +88,7 @@ export default function GradesPage({ registration }: { registration: string }) {
         tab={tab}
       >
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <Grades data={data} partialLoading={partialLoading} />
+          <Grades bond={bond} partialLoading={partialLoading} />
         </Box>
       </HomeProviders>
     </>
@@ -112,172 +100,3 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   };
 }
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.primary.dark,
-    //backgroundColor: theme.palette.grey[900],
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    //backgroundColor: theme.palette.primary.main,
-    backgroundColor: theme.palette.grey[900],
-  },
-  width: "100%",
-
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
-
-function formatGradesIndex(data: Bond[]) {
-  const gradesIndex: string[] = [];
-  data?.map(({ courses }) =>
-    courses?.map((course, key) =>
-      course.grades?.map((gradeGroup) => {
-        const exists = gradesIndex.includes(gradeGroup.name);
-        if (!exists) gradesIndex.push(gradeGroup.name);
-      })
-    )
-  );
-  const gradesIndexSorted = [
-    ...gradesIndex.sort((a, b) => {
-      if (a === "Resultado Final") {
-        return 1;
-      } else if (b === "Resultado Final") {
-        return -1;
-      } else if (a === "Recuperação") {
-        return 2;
-      } else if (b === "Recuperação") {
-        return -2;
-      } else {
-        return a < b ? -1 : 1;
-      }
-    }),
-  ];
-  return gradesIndexSorted;
-}
-function Grades({
-  data,
-  partialLoading,
-}: {
-  data: Bond[];
-  partialLoading: boolean;
-}) {
-  const gradesIndex = formatGradesIndex(data);
-
-  return (
-    <React.Fragment>
-      <CollapsibleTable>
-        <TableHead>
-          <StyledTableRow>
-            <StyledTableCell />
-            <StyledTableCell>Matéria</StyledTableCell>
-            {gradesIndex.map((index, key) => (
-              <StyledTableCell key={key}>{index}</StyledTableCell>
-            ))}
-          </StyledTableRow>
-        </TableHead>
-        <TableBody>
-          {data?.map(({ courses }) =>
-            courses?.map((course, key) => (
-              <Row course={course} key={key} gradesIndex={gradesIndex} />
-            ))
-          )}
-        </TableBody>
-      </CollapsibleTable>
-      <Box display={"flex"} justifyContent={"center"}>
-        {partialLoading ? (
-          <CircularProgress style={{ margin: "1rem" }} />
-        ) : (
-          <p></p>
-        )}
-      </Box>
-    </React.Fragment>
-  );
-}
-
-function Row({
-  course,
-  gradesIndex,
-}: {
-  course: Course;
-  gradesIndex: string[];
-}) {
-  const [open, setOpen] = React.useState(false);
-
-  return (
-    <>
-      <StyledTableRow sx={{ "& > *": { borderBottom: "unset" } }}>
-        <StyledTableCell>
-          <IconButton
-            aria-label="expand row"
-            size="medium"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
-          </IconButton>
-        </StyledTableCell>
-        <StyledTableCell component="th" scope="row">
-          {course.title}
-        </StyledTableCell>
-        {gradesIndex.map((index, key) => {
-          const realIndex = course?.grades?.find(
-            (gradeGroup) => gradeGroup.name === index
-          );
-          return (
-            <StyledTableCell align="left" key={key}>
-              <span style={{ fontSize: "1.0rem" }}>
-                {realIndex ? realIndex?.value?.toPrecision(2) : " "}
-              </span>
-            </StyledTableCell>
-          );
-        })}
-      </StyledTableRow>
-      <StyledTableRow>
-        <StyledTableCell
-          style={{ paddingBottom: 0, paddingTop: 0, border: 0 }}
-          colSpan={6}
-        >
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="caption" gutterBottom component="div">
-                Notas
-              </Typography>
-              <Table>
-                <TableHead>
-                  <StyledTableRow>
-                    {course?.grades?.map((gradeGroup, key) =>
-                      gradeGroup.grades?.map((grade, key) => (
-                        <StyledTableCell key={key}>
-                          {grade?.name}
-                        </StyledTableCell>
-                      ))
-                    )}
-                  </StyledTableRow>
-                </TableHead>
-                <TableBody>
-                  <StyledTableRow>
-                    {course?.grades?.map((gradeGroup, key) =>
-                      gradeGroup.grades?.map((grade, key) => (
-                        <StyledTableCell key={key}>
-                          {grade.value?.toPrecision(2)}
-                        </StyledTableCell>
-                      ))
-                    )}
-                  </StyledTableRow>
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </StyledTableCell>
-      </StyledTableRow>
-    </>
-  );
-}
