1b263ac25f16b5c652dec438ab4667f80f6e8c66
Gabriel Kleemann Duarte <gabrielkduarte234@gmail.com>
2022-08-06 11:07:57 -0300

    linha de notas mostra o peso e o valor maximo

--- components/Grades/Mobile/GradeRowMobile.tsx
+++ components/Grades/Mobile/GradeRowMobile.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { GradeGroup } from "@types";
 import {
   Box, Collapse,
   IconButton, TableBody, Typography
@@ -7,6 +6,7 @@ import {
 import CollapsibleTable from "@components/Home/CollapsibleTable";
 import { KeyboardArrowUp, KeyboardArrowDown } from "@material-ui/icons";
 import { StyledTableRow, StyledTableCell } from "../Content";
+import { GradeGroup, SumOfGrades, WeightedAverage } from "@types";
 
 export function GradeRowMobile(props: { grades: GradeGroup[]; gradesIndex: string[]; index: string; }) {
   const { grades, index } = props;
@@ -24,6 +24,19 @@ export function GradeRowMobile(props: { grades: GradeGroup[]; gradesIndex: strin
   );
   const hideGradeGroup = (!gradeGroup?.value || (gradeGroup?.subGrades?.length === 0 && gradeGroup.type !== "only-average")) ? grades.indexOf(gradeGroup as GradeGroup) !== 0 : false;
   if (hideGradeGroup) return null;
+  const weights = [] as number[];
+  if (gradeGroup?.type === "weighted-average") {
+    const subGrades = gradeGroup.subGrades as WeightedAverage[]
+    subGrades.map((subGrade) => weights.push(subGrade.weight))
+  }
+  const maxValues = [] as number[];
+  if (gradeGroup?.type === "sum-of-grades") {
+    const subGrades = gradeGroup.subGrades as SumOfGrades[]
+    subGrades.map((subGrade) => maxValues.push(subGrade.maxValue))
+  }
+  function GradeRow() {
+    return
+  }
   return (
     <>
       <StyledTableRow>
@@ -53,27 +66,33 @@ export function GradeRowMobile(props: { grades: GradeGroup[]; gradesIndex: strin
           <Collapse in={open && subGrade} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1, mb: 3 }}>
               <Typography variant="caption" gutterBottom component="div">
-                Notas {gradeGroup?.type === "sum-of-grades" ? "da soma" : "da média"}
+                Notas {gradeGroup?.type === "only-average" ? "da média" : (gradeGroup?.type === "sum-of-grades" ? "da soma" : "da média ponderada")}
               </Typography>
               <CollapsibleTable>
                 <TableBody>
-                  {gradeGroup?.subGrades?.map((grade, key) => {
-                    if (grade.value === undefined) return null;
-                    return (
-                      <StyledTableRow key={key}>
-                        <StyledTableCell>
-                          <Typography fontSize={".9rem"}>
-                            {grade?.name}
-                          </Typography>
-                        </StyledTableCell>
-                        <StyledTableCell align="center">
-                          <Typography fontSize={"1rem"}>
-                            {grade.value !== undefined ? grade?.value?.toPrecision(2) : "-"}
-                          </Typography>
-                        </StyledTableCell>
-                      </StyledTableRow>
-                    );
-                  })}
+                  {gradeGroup?.subGrades.map((grade, key) => {
+                      if (grade.value === undefined) return <></>;
+                      return (
+                        <StyledTableRow key={key}>
+                          <StyledTableCell>
+                            <Typography fontSize={".9rem"}>
+                              {grade.name}
+                              <Typography variant="caption" style={{
+                                lineBreak: "loose",
+                                whiteSpace: "nowrap"
+                              }}>{gradeGroup.type === "weighted-average" ? ` (Peso ${weights[key]})` : null}
+                                {gradeGroup.type === "sum-of-grades" ? ` (Máx ${maxValues[key]})` : null}</Typography>
+                            </Typography>
+                          </StyledTableCell>
+                          <StyledTableCell align="center">
+                            <Typography fontSize={"1rem"}>
+                              {grade.value !== undefined ? grade?.value?.toPrecision(2) : "-"}
+                            </Typography>
+                          </StyledTableCell>
+                        </StyledTableRow>
+                      );
+                    })
+                  }
                 </TableBody>
               </CollapsibleTable>
             </Box>
